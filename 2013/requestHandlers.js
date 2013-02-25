a135b1dd6f86f8291ca2484517ca83357fc5746c
Alex Stout <astout@goalzero.com>
2013-02-25 15:51:03 -0700

    Inventory Page Demo Complete
    Completed the inventory page demo.  The user is able to add and remove
    inventory items to the database.  The inventory is listed below the
    form and is pseduo live.

---
+++ 2013/requestHandlers.js
@@ -0,0 +1,175 @@
+//requestHandlers.js
+
+var querystring = require("querystring"),
+    fs = require("fs"),
+    formidable = require("formidable");
+
+function start(response) {
+  console.log("Request handler 'start' was called.");
+
+  var body = '<html>'+
+    '<head>'+
+    '<h1>Upload an image file</h1>' +
+    '<meta http-equiv="Content-Type" '+
+    'content="text/html; charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" enctype="multipart/form-data" '+
+    'method="post">'+
+    '<input type="file" name="upload" multiple="multiple">'+
+    '<input type="submit" value="Upload file" />'+
+    '</form>'+
+    '<p>Or go to the <a href ="./form"/> Form </a></p>' +
+    '</body>'+
+    '</html>';
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+}
+
+function upload(response, request) {
+  console.log("Request handler 'upload' was called.");
+
+  var form = new formidable.IncomingForm();
+  console.log("about to parse");
+  form.parse(request, function(error, fields, files) {
+    console.log("parsing done");
+
+    /* Possible error on Windows systems:
+       tried to rename to an already existing file */
+    fs.rename(files.upload.path, "/tmp/test.png", function(err) {
+      if (err) {
+        fs.unlink("/tmp/test.png");
+        fs.rename(files.upload.path, "/tmp/test.png");
+      }
+    });
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("received image:<br/>");
+    response.write("<img src='/show' />");
+    response.end();
+  });
+}
+
+function show(response) {
+  console.log("Request handler 'show' was called.");
+  fs.readFile("/tmp/test.png", "binary", function(error, file) {
+    if(error) {
+      response.writeHead(500, {"Content-Type": "text/plain"});
+      response.write(error + "\n");
+      response.end();
+    } else {
+      response.writeHead(200, {"Content-Type": "image/png"});
+      response.write(file, "binary");
+      response.end();
+    }
+  });
+}
+
+function form(response, request, collection, url) {
+
+  console.log("Request handler 'form' was called");
+  var FieldQuery = url.parse(request.url,true).query;
+
+  if(!FieldQuery.hasOwnProperty('Event'))
+  {
+    fs.readFile('./form1.html', function (err, html) 
+    {
+      if(err)
+      {
+         response.writeHead(404, {"Content-Type": "text/plain"});
+         response.write(error + "\n");
+         response.end();
+      }
+      else 
+      {
+        response.writeHead(200, {"Content-Type": "text/html"});       
+        response.write(html);
+        response.end();
+        console.log('loading form');
+      }
+    });
+  }
+
+  if(FieldQuery.Event=='Insert')
+  {
+    collection.insert({'sku': FieldQuery.sku, 'desc':FieldQuery.desc});
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(FieldQuery.sku +' - ' + FieldQuery.desc + ' successfully added');
+    response.end();
+  }
+
+  if(FieldQuery.Event=='Remove')
+  {
+    collection.findAndRemove({'sku': FieldQuery.sku, 'desc': FieldQuery.desc},
+      function(error, result)
+    {
+      if(result!=null)
+      {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write(FieldQuery.sku + ' - ' + FieldQuery.desc +' successfully removed');
+        response.end();
+      }
+      else
+      {
+        console.log('!--Remove failed--!');
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write('Failure to Remove \' '+ FieldQuery.sku + ' - ' + FieldQuery.desc + ' \' : Unit not found.');
+        response.end();
+      }
+    });
+  }
+
+  if(FieldQuery.Event=='List')
+  {
+    collection.find().toArray(
+      function (error, doc)
+      {
+        if(doc.length)
+        {
+          response.writeHead(200, {"Content-Type": "text/plain"});
+          response.write('<table width = "400px" border = "1" ALIGN = center>' +
+                            '<tr>' +
+                            '<td style = "background-color:#E0E0E0; width: 90px; text-align:center"> <b>SKUs</b>' +
+                            '</td>' +
+                            '<td style = "background-color:#E0E0E0; text-align:center"> <b>Product Description</b>' +
+                            '</td>' +
+                            '</tr>');
+          for(i=0;i<doc.length;i++)
+          {
+            response.write('<tr>' +
+              '<td style = "background-color:#F0F0F0; width:90px; vertical-align:top;">' +
+              doc[i].sku +
+              '</td>' +
+              '<td style = "background-color:#F0F0F0; vertical-align:top;">' +
+              doc[i].desc +
+              '</td>' + 
+              '</tr>'
+              );
+
+            console.log("find() results: " + doc[i].sku + " : " + doc[i].desc + "   #" + i);
+          }
+          response.write('</table>');
+          response.end();
+        }
+        else
+        {
+          response.writeHead(200, {"Content-Type": "text/plain"});
+          response.write('<table width = "400px" border = "1" ALIGN = center>' +
+          '<tr>' +
+          '<td style = "background-color:#E0E0E0; width: 90px; text-align: center">' +
+          '<b>---  Inventory Empty ---</b>' +
+          '</td>' +
+          '</tr>' +
+          '</table>');
+          response.end();
+        }
+      });
+  }
+
+}
+
+exports.start = start;
+exports.upload = upload;
+exports.show = show;
+exports.form = form;
---
+++ 2013/server.js
@@ -0,0 +1,66 @@
+//server.js
+
+var http = require("http"),
+	url = require("url"),
+	port = 80;
+	mongo = require('mongodb'),
+	Server = mongo.Server,
+	MongoServer = new Server('localhost', 27017, 
+		{
+			auto_reconnect: true
+		}),
+	Db = mongo.Db,
+	dbName = 'GZ',
+	collectionName = 'DataBase',
+	db = new Db(dbName, MongoServer);
+var OpenDB, 
+	DBCollection;
+
+
+/**
+  * Request Handlers
+ */
+var requestHandlers = require("./requestHandlers");
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
+handle["/form"] = requestHandlers.form;
+
+db.open(function(error, db)
+	{
+		OpenDB = db;
+		db.collection(collectionName, 
+			function(error, collection)
+			{
+				DBCollection = collection;
+				console.dir(MongoServer);
+				console.log('\n\n\n\t' + '\x1b[31;1m' + 'The server has started on' + '\x1b[32;1m Port: ' + port + '\n\n\n' + '\033[0m');
+				console.log('\n\nDatabase used : \x1b[33;1m\''+dbName+'\' \033[0m \t Collection used: \x1b[33;1m\''+collectionName+'\'\033[0m\n');
+			})
+	});	
+
+function start(route, handle) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+    route(handle, pathname, response, request, DBCollection, url);
+  }
+
+  http.createServer(onRequest).listen(port);
+}
+
+function route(handle, pathname, response, request, collection, url) {
+  console.log("About to route a request for " + pathname);
+  if (typeof handle[pathname] === 'function') {
+    handle[pathname](response, request, collection, url);
+  } else {
+    console.log("No request handler found for " + pathname);
+    response.writeHead(404, {"Content-Type": "text/html"});
+    response.write("404 Not found");
+    response.end();
+  }
+}
+
+start(route, handle);
